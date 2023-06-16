5235f694ee5a42bd724d1f68338cf7638a16dc14
rexliu-hub <59669419+rexliu-hub@users.noreply.github.com>
2023-06-16 00:02:18 -0700

    Update Project 2 SQL query for Tableau Visualization.sql

--- Project 2 SQL query for Tableau Visualization.sql
+++ Project 2 SQL query for Tableau Visualization.sql
@@ -1,11 +1,11 @@
 
---1.
+--Graph1: Global Numbers
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From [Project 1]..CovidDeaths$
 where continent is not null 
 order by 1,2
 
---2.
+--Graph2: Total Death Per Continent
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From [Project 1]..CovidDeaths$
 Where continent is null 
@@ -13,13 +13,13 @@ and location not in ('World', 'European Union', 'International')
 Group by location
 order by TotalDeathCount desc
 
--- 3.
+--Graph3: Percent Population Infected Per Country
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From [Project 1]..CovidDeaths$
 Group by Location, Population
 order by PercentPopulationInfected desc
 
--- 4.
+--Graph4: Percent Population Infected
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From [Project 1]..CovidDeaths$
 Group by Location, Population, date
